eb74b977b93db2bba386687587636c2a39b6096a
O-Tahmas <ot.tahmas@gmail.com>
2024-05-25 13:57:01 +0100

    Added type tables scripting

---
+++ scripts/pokemon_db_setup.py
@@ -0,0 +1,96 @@
+"""
+DOCS HERE ON SETTING UP PostgreSQL Database
+"""
+import requests
+import psycopg2
+import json
+
+def connect_db():
+    conn = psycopg2.connect(
+        dbname="pokemon_db",
+        user="postgres", # default values, change as per your local config
+        password="admin", # default values, change as per your local config
+        host="localhost"
+    )
+    return conn
+
+def fetch_pokemon_types():
+    url = "https://pokeapi.co/api/v2/type/"
+    response = requests.get(url)
+    types_data = response.json()
+    return types_data['results']
+
+# Get type data from the API result
+def get_type_data(type_api_result):
+    response = requests.get(type_api_result['url'])
+    response_json = response.json()
+
+    type_id = response_json['id']
+    name = response_json['name']
+    generation = response_json['generation']['url'].split('/')[-2]
+
+    damage_relations = []
+    for relation, related_types in response_json['damage_relations'].items():
+        multiplier = {
+            "no_damage_to": 0.0,
+            "half_damage_to": 0.5,
+            "double_damage_to": 2.0,
+            "no_damage_from": 0.0,
+            "half_damage_from": 0.5,
+            "double_damage_from": 2.0
+        }[relation]
+
+        for related_type in related_types:
+            related_type_id = int(related_type['url'].split('/')[-2])
+            damage_relations.append((type_id, related_type_id, multiplier))
+
+    return [type_id, name, generation, damage_relations]
+
+# Insert type data into the database
+def insert_type_data(conn, type_id, name, generation):
+    cur = conn.cursor()
+    cur.execute("""
+        INSERT INTO pkmn_types (type_id, type_name, generation)
+        VALUES (%s, %s, %s)
+        ON CONFLICT (type_id) DO NOTHING
+    """, (type_id, name, generation))
+    conn.commit()
+    cur.close()
+
+# Insert damage relations into the database
+# Only relations that are <> 1 are inserted, to save space
+def insert_damage_relations(conn, damage_relations):
+    cur = conn.cursor()
+    for type_id, related_type_id, multiplier in damage_relations:
+        cur.execute("""
+            INSERT INTO type_relations (type_id, related_type_id, dmg_multiplier)
+            VALUES (%s, %s, %s)
+            ON CONFLICT (type_id, related_type_id) DO NOTHING
+        """, (type_id, related_type_id, multiplier))
+    conn.commit()
+    cur.close()
+
+def main():
+    # Connect to the database
+    conn = connect_db()
+
+    # Fetch and insert types and relations
+    pokemon_types = fetch_pokemon_types()
+    type_entries = []
+    for type_api_result in pokemon_types:
+        type_entries.append( get_type_data(type_api_result) )
+    
+    # Need to loop through twice, so that PostGres allows for foriegn insertions properly
+    for entry in type_entries:
+        type_id, name, generation = entry[0], entry[1], entry[2]
+        insert_type_data(conn, type_id, name, generation)
+    
+    for entry in type_entries:
+        damage_relations = entry[3]
+        insert_damage_relations(conn,damage_relations)
+
+    # Close the database connection
+    conn.close()
+
+if __name__ == "__main__":
+    main()
