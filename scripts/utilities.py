9ee23f604587095000c2f0055e920feac6101ec1
O-Tahmas <ot.tahmas@gmail.com>
2024-05-29 20:39:03 +0100

    Implemented tests around utilities and basic schema functionality

--- scripts/utilities.py
+++ scripts/utilities.py
@@ -9,20 +9,18 @@ def load_config(config_file="config.yaml"):
         config = yaml.safe_load(file)
     return config
 
-
-def connect_db():
+def connect_db(dbname=None):
     config = load_config()
     db_config = config["database"]
 
     conn = psycopg2.connect(
-        dbname=db_config["dbname"],
+        dbname=dbname or db_config["dbname"],
         user=db_config["user"],
         password=db_config["password"],
         host=db_config["host"],
         port=db_config["port"],
     )
     return conn
-
 
 # Stop trying to make `fetch` happen, it's not going to happen!
 def fetch_all(url):
